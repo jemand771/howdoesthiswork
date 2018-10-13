38fbf33ec4c1e48b5ebd22a94b035d5dba0502ca
jemand771 <jemand771@gmx.net>
2018-10-13 16:37:48 +0200

    added add function, made divide() non-public

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,18 +2,23 @@ package main;
 
 public class Main {
 
-	
-	public static void main(String[] args) {
-		
-		System.out.println("so this works for now");
-		System.out.println("furthermore i changed stuff");
 
-		System.out.println("now im supposed to be nice :)");
-		System.out.println("8 / 2 = " + divide(8, 2));
-	}
-	
-	public static int divide(int x, int y) {
-		
-		return x / y;
-	}
+    public static void main(String[] args) {
+
+        System.out.println("so this works for now");
+        System.out.println("furthermore i changed stuff");
+
+        System.out.println("now im supposed to be nice :)");
+        System.out.println("8 / 2 = " + divide(8, 2));
+    }
+
+    static int add(int x, int y) {
+
+        return x + y;
+    }
+
+    static int divide(int x, int y) {
+
+        return x / y;
+    }
 }
